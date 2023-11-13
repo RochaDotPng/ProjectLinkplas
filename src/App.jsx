70d183ce5470745cf58807ebbd0aeaf997ccda78
Andre Silva <andre.rochasilva@outlook.pt>
2023-11-13 17:04:24 +0000

    Header development

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-import Body from './components/Body'
+import Header from './components/Header'
 
 function App() {
 
   return (
-    <Body />
+    <Header />
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button'
+
+function Header() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src="./logo.png"
+            width="139"
+            height="73"
+            className="d-inline-block align-top"
+          />{' '}
+        </Navbar.Brand>
+        <div className="justify-content-end">
+          <Nav className="gap-4 me-auto">
+            <Nav.Link href="#home">Sobre</Nav.Link>
+            <Nav.Link href="#link">Serviços</Nav.Link>
+            <Nav.Link href="#link">Galeria</Nav.Link>
+            <Nav.Link href="#link">Politica de Qualidade</Nav.Link>
+            <Button>Contactos</Button>
+          </Nav>
+        </div> 
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavSection;
