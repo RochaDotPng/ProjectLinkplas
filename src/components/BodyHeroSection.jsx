87cf8e05e9045c617d76ec098f1f61f3f03648b9
André Silva <andre.rochasilva@outlook.pt>
2023-11-13 18:39:43 +0000

    New Image and Carousel

--- src/components/BodyHeroSection.jsx
+++ src/components/BodyHeroSection.jsx
@@ -1,9 +1,32 @@
+import Carousel from 'react-bootstrap/Carousel';
 
 export default function BodyHeroSection() {
     return (
-        <div className="main--hero-section">
-            <div className="main--hero-image"></div>
-        </div>
+        <Carousel>
+            <Carousel.Item>
+                <img src='./images/company-front.jpg'></img>
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+            <img src='./images/company-front.jpg'></img>
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+            <img src='./images/company-front.jpg'></img>
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     )
 }
 
