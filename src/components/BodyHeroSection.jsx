f9d390f2849e9282b5bac7a05d8a3ed6ce66b1be
André Silva <andre.rochasilva@outlook.pt>
2023-11-13 20:07:48 +0000

    Final changes to Hero section and some css aditions

--- src/components/BodyHeroSection.jsx
+++ src/components/BodyHeroSection.jsx
@@ -2,19 +2,19 @@ import Carousel from 'react-bootstrap/Carousel';
 
 export default function BodyHeroSection() {
     return (
-        <Carousel>
+        <Carousel className='carousel'>
             <Carousel.Item>
-                <img src='./images/company-front.jpg'></img>
+                <img className='carousel-img' src='./images/company-front-carousel.jpg'></img>
                 <Carousel.Caption>
-                    <h2 className='fw-bold'>LINKPLAS</h2>
-                    <p>Uma empresa orientada para a excelência</p>
+                    <h1 className='fw-bold'>LINKPLAS</h1>
+                    <p className='fs-5' >Uma empresa orientada para a excelência</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-            <img src='./images/machine-front.jpg'></img>
+                <img className="carousel-img" src='./images/machine-front-carousel.jpg'></img>
                 <Carousel.Caption>
-                    <h3 className='fw-bold'>LINKPLAS</h3>
-                    <p>Tudo sobre plásticos</p>
+                    <h1 className='fw-bold'>LINKPLAS</h1>
+                    <p className='fs-5' >Tudo sobre plásticos</p>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
