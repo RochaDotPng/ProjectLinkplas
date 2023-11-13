629ba57599d01600901e8224f0eb639bac38b748
joseseica <92593792+joseseica@users.noreply.github.com>
2023-11-13 16:55:00 +0000

    alterações, criação da pasta pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import './custom.scss';
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = document.getElementById("root");
+const reactRoot = createRoot(root);
+
+reactRoot.render(<App/>);
