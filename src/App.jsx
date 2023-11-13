0ae2e345f604305411d66f162244c42af32a7f88
joseseica <92593792+joseseica@users.noreply.github.com>
2023-11-13 17:54:32 +0000

    adição dos path na app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './pages/Home'
+import About from './pages/About'
+import Contacts from './pages/Contacts'
+import Gallery from './pages/Gallery'
+import Policy from './pages/Policy'
+import Services from './pages/Services'
 
-function App() {
+export default function App() {
 
   return (
-    <Home/>
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/About" element={<About/>}/>
+      <Route path="/Contacts" element={<Contacts/>}/>
+      <Route path="/Gallery" element={<Gallery/>}/>
+      <Route path="/Policy" element={<Policy/>}/>
+      <Route path="/Services" element={<Services/>}/>
+    </Routes>
+    </BrowserRouter>
+    </>
   )
 }
-
-export default App
