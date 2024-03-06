d931bd6974db540e73fc4bc7804ad1d54e0b96a9
joseseica <92593792+joseseica@users.noreply.github.com>
2024-03-06 18:53:57 +0000

    commit do aço 2

--- src/components/BodyProductsSection.jsx
+++ src/components/BodyProductsSection.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import ProductButton from './ProductButton';
 import Container from 'react-bootstrap/Container';
 
