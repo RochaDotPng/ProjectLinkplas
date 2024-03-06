4f91d9b36b6dcd292a37e1626daf76f449feac09
André Silva <andre.rochasilva@outlook.pt>
2024-03-06 14:57:00 +0000

    Reverted to Carousel, but changed to the new image

--- src/components/BodyHeroSection.jsx
+++ src/components/BodyHeroSection.jsx
@@ -3,12 +3,22 @@ import Carousel from 'react-bootstrap/Carousel';
 export default function BodyHeroSection() {
     return (
         <>
-            <div className='main--hero-section'>
-                <div className='width-700'>
-                    <h1 className='fw-bold '>LINKPLAS</h1>
-                    <p className='fs-5 text-wrap' >Juntos, criamos soluções em plástico que não apenas transformam ideias em realidade, mas também cuidam do nosso planeta. Descubra uma parceria que vai além da inovação, moldando um amanhã mais verde, um projeto de cada vez</p>
-                </div>
-            </div>
+        <Carousel interval="4000" className='carousel'>
+            <Carousel.Item>
+                <img className='carousel-img' src='./images/home-hero-landscape.jpg'></img>
+                <Carousel.Caption>
+                    <h1 className='fw-bold'>LINKPLAS</h1>
+                    <p className='fs-5' >Juntos, criamos soluções em plástico que não apenas transformam ideias em realidade, mas também cuidam do nosso planeta. Descubra uma parceria que vai além da inovação, moldando um amanhã mais verde, um projeto de cada vez</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img className="carousel-img" src='./images/tupperLink-hero.png'></img>
+                <Carousel.Caption>
+                    <h1 className='fw-bold'>TupperLink</h1>
+                    <p className='fs-5' >Take-away economico e reutilizavel</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
         </>
     )
 }
