4d6a4ced6ad6c9e822f80a170af37e9910b2b929
joseseica <92593792+joseseica@users.noreply.github.com>
2024-03-07 14:39:01 +0000

    commit do aço

--- src/components/BodyPolicySection.jsx
+++ src/components/BodyPolicySection.jsx
@@ -8,7 +8,7 @@ export default function BodyPolicySection() {
                 </div>
                 <div className="policy-text-container">
                     <h1 className="fw-bold mb-4 mt-5">Estratégia</h1>
-                    <p>Através de uma estratégia de Melhoria Contínua, a Linkplas compromete-se a desenvolver e implementar continuamente medidas e ações necessárias no sentido de manter os padrões de qualidade, reduzir e minimizar perigos e riscos da sua atividade tomando todas as medidas necessárias.</p>                    
+                    <p>Através de uma estratégia de Melhoria Contínua, a Linkplas compromete-se a desenvolver e implementar continuamente medidas e ações necessárias no sentido de manter os padrões de qualidade, reduzir e minimizar perigos e riscos da sua atividade tomando todas as medidas necessárias.</p>
                     <a className='pointer text-decoration-none d-flex align-items-center text-primary fw-semibold'>Saber mais<i className="bi text-primary bi-arrow-right ms-2"></i></a>
                     <div className="policy-quote-container">
                         <p>Na Linkplas, acreditamos que a qualidade é mais do que um compromisso - é a essência do nosso DNA empresarial. Guiados pela inovação e pela busca constante pela excelência, estamos empenhados em não apenas atender, mas superar as expectativas dos nossos clientes.</p>
--- src/components/about-components/GreenCard.jsx
+++ src/components/about-components/GreenCard.jsx
@@ -1,9 +1,10 @@
-export default function GreenCard(){
-    return(
+export default function GreenCard() {
+    return (
         <div className="about-green-card">
             <p>A Linkplas é uma empresa industrial de produção e comercialização de plásticos para peças industriais, e para outros fins. Somos uma organização dinâmica, moderna e dedicada na satisfação das necessidades dos nossos clientes</p>
-            <p>Paula Rocha</p>
-            <p className="ceo">CEO</p>
+            <span className="fw-bold">Paula Rocha</span>
+            <br />
+            <span className="policy-quote-text">CEO</span>
         </div>
     )
 }
