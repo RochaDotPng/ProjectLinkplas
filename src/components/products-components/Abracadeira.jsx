5a0d9c754180af0c2db15d5c8b294a3e576ca2ad
André Silva <andre.rochasilva@outlook.pt>
2024-03-07 11:41:38 +0000

    File download on other products

--- src/components/products-components/Abracadeira.jsx
+++ src/components/products-components/Abracadeira.jsx
@@ -1,4 +1,4 @@
-import { Button, Container } from 'react-bootstrap';
+import { Button, Container, Dropdown } from 'react-bootstrap';
 
 export default function Abracadeira(){
     return(
@@ -7,7 +7,22 @@ export default function Abracadeira(){
             <div className="product-container">
                 <div className='product-text-container'>
                     <p>A inovadora Abraçadeira para Tubo da Linkplas: a solução perfeita para fixação eficiente e de confiança. Com um design inteligente e encaixe fácil, nossa abraçadeira de plástico proporciona uma instalação sem complicações, enquanto a sua resistência garante segurança duradoura. Moldada com precisão, a nossa abraçadeira é a escolha ideal para garantir a estabilidade e integridade dos seus projetos.</p>
-                    <div><Button className='p-3'>Pedir cotação</Button><Button className='p-3 btn-secondary ms-3 text-white'>Especificações<i className='text-white ms-2 bi bi-download'></i></Button></div>
+                    <div>
+                        <Button className='p-3'>Pedir cotação</Button>
+                        <Dropdown className='products-download d-inline'>
+                            <Dropdown.Toggle className="p-3 btn-secondary ms-3 text-white" variant="secondary" id="dropdown-basic">
+                            <span>Descarregar</span><i className='text-white ms-2 bi bi-download'></i>
+                            </Dropdown.Toggle>
+
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="./files/catalogo_Linkplas_sem_taco.pdf" download>Catalogo</Dropdown.Item>
+                                <Dropdown.Item href="./files/Abracadeira/7017030008.DWG" download>2D - DWG </Dropdown.Item>
+                                <Dropdown.Item href="./files/Abracadeira/7017030008.IGS" download>3D - IGS</Dropdown.Item>
+                                <Dropdown.Item href="./files/Abracadeira/7017030008.STEP" download>3D - STEP</Dropdown.Item>
+                                <Dropdown.Item href="./files/Abracadeira/7017030008.SLDPRT" download>3D - SLDPRT</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
                     <img className='perna-animation' src='./images/perna_animation.gif'></img>
                 </div>
                 <div className="product-img-container">
--- src/components/products-components/Intercalar.jsx
+++ src/components/products-components/Intercalar.jsx
@@ -1,4 +1,4 @@
-import { Button, Container, Table } from 'react-bootstrap';
+import { Button, Container, Dropdown } from 'react-bootstrap';
 
 
 export default function Intercalar() {
@@ -12,7 +12,21 @@ export default function Intercalar() {
                 <div className='product-text-container'>
                     <h1 className="fw-bold mb-2 ">Intercalar longarina</h1>
                     <p>Espaçador para longarinas em transportadores.</p>
-                    <div><Button className='p-3'>Pedir cotação</Button><Button className='p-3 btn-secondary ms-3 text-white'>Especificações<i className='text-white ms-2 bi bi-download'></i></Button></div>
+                    <div>
+                        <Button className='p-3'>Pedir cotação</Button>
+                        <Dropdown className='products-download d-inline'>
+                            <Dropdown.Toggle className="p-3 btn-secondary ms-3 text-white" variant="secondary" id="dropdown-basic">
+                            <span>Descarregar</span><i className='text-white ms-2 bi bi-download'></i>
+                            </Dropdown.Toggle>
+
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="" download>2D - DWG </Dropdown.Item>
+                                <Dropdown.Item href="" download>3D - IGS</Dropdown.Item>
+                                <Dropdown.Item href="" download>3D - STEP</Dropdown.Item>
+                                <Dropdown.Item href="" download>3D - SLDPRT</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
                 </div>
             </div>
         </Container>
