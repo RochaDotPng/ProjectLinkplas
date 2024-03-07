b4dc904814c2bffba09fbfeb4706b90a27f02c87
joseseica <92593792+joseseica@users.noreply.github.com>
2024-03-07 18:36:25 +0000

    commit do aço

--- src/App.jsx
+++ src/App.jsx
@@ -16,19 +16,19 @@ export default function App() {
 
   return (
     <>
-        <TransitionGroup>
-          <BrowserRouter basename="/">
-            <Routes>
-              <Route path="/" element={<SwitchTransition><CSSTransition key="home" classNames="page-fade" timeout={500}><Home /></CSSTransition></SwitchTransition>} />
-              <Route path="/About" element={<SwitchTransition><CSSTransition key="about" classNames="page-fade" timeout={500}><About /></CSSTransition></SwitchTransition>} />
-              <Route path="/Contacts" element={<SwitchTransition><CSSTransition key="contacts" classNames="page-fade" timeout={500}><Contacts /></CSSTransition></SwitchTransition>} />
-              <Route path="/Gallery" element={<SwitchTransition><CSSTransition key="gallery" classNames="page-fade" timeout={500}><Gallery /></CSSTransition></SwitchTransition>} />
-              <Route path="/Policy" element={<SwitchTransition><CSSTransition key="policy" classNames="page-fade" timeout={500}><Policy /></CSSTransition></SwitchTransition>} />
-              <Route path="/Products/:parameter" element={<SwitchTransition><CSSTransition key="products" classNames="page-fade" timeout={500}><Products /></CSSTransition></SwitchTransition>} />
-              <Route path="/Products" element={<SwitchTransition><CSSTransition key="products" classNames="page-fade" timeout={500}><Products /></CSSTransition></SwitchTransition>} />
-            </Routes>
-          </BrowserRouter>
-        </TransitionGroup>
+      <TransitionGroup>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<SwitchTransition><CSSTransition key="home" classNames="page-fade" timeout={500}><Home /></CSSTransition></SwitchTransition>} />
+            <Route path="/About" element={<SwitchTransition><CSSTransition key="about" classNames="page-fade" timeout={500}><About /></CSSTransition></SwitchTransition>} />
+            <Route path="/Contacts" element={<SwitchTransition><CSSTransition key="contacts" classNames="page-fade" timeout={500}><Contacts /></CSSTransition></SwitchTransition>} />
+            <Route path="/Gallery" element={<SwitchTransition><CSSTransition key="gallery" classNames="page-fade" timeout={500}><Gallery /></CSSTransition></SwitchTransition>} />
+            <Route path="/Policy" element={<SwitchTransition><CSSTransition key="policy" classNames="page-fade" timeout={500}><Policy /></CSSTransition></SwitchTransition>} />
+            <Route path="/Products" element={<SwitchTransition><CSSTransition key="products" classNames="page-fade" timeout={500}><Products /></CSSTransition></SwitchTransition>} />
+            <Route path="/Products/:parameter" element={<SwitchTransition><CSSTransition key="products" classNames="page-fade" timeout={500}><Products /></CSSTransition></SwitchTransition>} />
+          </Routes>
+        </BrowserRouter>
+      </TransitionGroup>
     </>
 
   )
