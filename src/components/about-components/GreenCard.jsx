eab0997fc9fd0cd3080186ce5aa16b6c5c4ce660
joseseica <92593792+joseseica@users.noreply.github.com>
2024-03-07 15:25:15 +0000

    commit do aço

--- src/components/about-components/GreenCard.jsx
+++ src/components/about-components/GreenCard.jsx
@@ -1,10 +1,14 @@
+import { Container } from "react-bootstrap";
+
 export default function GreenCard() {
     return (
         <div className="about-green-card">
-            <p>A Linkplas é uma empresa industrial de produção e comercialização de plásticos para peças industriais, e para outros fins. Somos uma organização dinâmica, moderna e dedicada na satisfação das necessidades dos nossos clientes</p>
-            <span className="fw-bold">Paula Rocha</span>
-            <br />
-            <span className="policy-quote-text">CEO</span>
+            <Container>
+                <p>A Linkplas é uma empresa industrial de produção e comercialização de plásticos para peças industriais, e para outros fins. Somos uma organização dinâmica, moderna e dedicada na satisfação das necessidades dos nossos clientes</p>
+                <span className="fw-bold">Paula Rocha</span>
+                <br />
+                <span className="about-quote-text">CEO</span>
+            </Container>
         </div>
     )
 }
