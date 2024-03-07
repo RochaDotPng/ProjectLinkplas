16d3e219c9ed628d94835c374898bcc229a8020f
joseseica <92593792+joseseica@users.noreply.github.com>
2024-03-07 11:37:06 +0000

    card contacts

--- src/components/contacts-components/Card.jsx
+++ src/components/contacts-components/Card.jsx
@@ -1,16 +1,16 @@
 export default function Card() {
     return (
-      <div className="contacts-card">
-        <div className="icon-container">
-          <img src="./images/morada.png" alt="" />
+        <div className="contacts-card">
+            <h5>Morada:</h5>
+            <p>Linkplas, Lda</p>
+            <p>Rua António Gomes Correia Júnior</p>
+            <p>Zona Industrial do Mergulhão - Cesar</p>
+            <p>3700-606 Oliveira de Azeméis</p>
+            <p>Portugal</p>
+            <h5>Telefone:</h5>
+            <p>+351 256 601 535</p>
+            <h5>Email:</h5>
+            <p>paula.rocha@linkplas.pt</p>
         </div>
-        <div className="icon-container">
-          <img src="./images/telefone.png" alt="" />
-        </div>
-        <div className="icon-container">
-          <img src="./images/email.png" alt="" />
-        </div>
-      </div>
     );
-  }
-  
+}
