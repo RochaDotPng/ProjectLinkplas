283a535d21a30050893c938150f3f6841743341b
joseseica <92593792+joseseica@users.noreply.github.com>
2024-03-08 19:51:38 +0000

    commit do aço

--- src/components/BodyPolicySection.jsx
+++ src/components/BodyPolicySection.jsx
@@ -1,6 +1,15 @@
 import { Container } from "react-bootstrap";
+import { useNavigate } from 'react-router-dom'
 
 export default function BodyPolicySection() {
+
+    const navigate = useNavigate();
+
+    const handlePolicyButtonClick = (e) => {
+        e.preventDefault();
+        navigate('/Policy');
+    };
+
     return (
         <><Container>
             <div className="policy-container">
@@ -9,7 +18,7 @@ export default function BodyPolicySection() {
                 <div className="policy-text-container">
                     <h1 className="fw-bold mb-4 mt-5">Estratégia</h1>
                     <p>Através de uma estratégia de Melhoria Contínua, a Linkplas compromete-se a desenvolver e implementar continuamente medidas e ações necessárias no sentido de manter os padrões de qualidade, reduzir e minimizar perigos e riscos da sua atividade tomando todas as medidas necessárias.</p>
-                    <a className='pointer text-decoration-none d-flex align-items-center text-primary fw-semibold'>Saber mais<i className="bi text-primary bi-arrow-right ms-2"></i></a>
+                    <a onClick={handlePolicyButtonClick} className='pointer text-decoration-none d-flex align-items-center text-primary fw-semibold'>Saber mais<i className="bi text-primary bi-arrow-right ms-2"></i></a>
                     <div className="policy-quote-container">
                         <p>Na Linkplas, acreditamos que a qualidade é mais do que um compromisso - é a essência do nosso DNA empresarial. Guiados pela inovação e pela busca constante pela excelência, estamos empenhados em não apenas atender, mas superar as expectativas dos nossos clientes.</p>
                         <span className="fw-bold">Paula Rocha</span>
