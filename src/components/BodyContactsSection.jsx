32fda8e03442b41cdda79465e7f21e6006714524
joseseica <92593792+joseseica@users.noreply.github.com>
2024-03-11 20:36:37 +0000

    commit do aço

--- src/components/BodyContactsSection.jsx
+++ src/components/BodyContactsSection.jsx
@@ -2,12 +2,30 @@ import Container from 'react-bootstrap/Container';
 import FormCard from './contacts-components/FormCard'
 import Card from './contacts-components/Card'
 import GreenCard from './contacts-components/GreenCard'
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 export default function BodyContactsSection() {
+
+    const mapContainerStyle = {
+        height: '100vh',
+        width: '100%',
+    };
+
+    const center = {
+        lat: 40.89962330345354,
+        lng: -8.43715279187227,
+    };
+
     return (
-        <div className="contacts-body-container">
-            <div className="contacts-body-container-img">
+        <LoadScript googleMapsApiKey="AIzaSyCzB2FC7gcUk4ceAPWiDyVSuPn6e7uAygo">
+            <div className="contacts-body-container">
                 <Container className="content-container">
+                    <GoogleMap
+                        mapContainerStyle={mapContainerStyle}
+                        zoom={15}
+                        center={center}
+                    >
+                    </GoogleMap>
                     <div className="card-overlay">
                         <Card />
                     </div>
@@ -15,14 +33,12 @@ export default function BodyContactsSection() {
                         <FormCard />
                     </div>
                 </Container>
-
                 <div className="green-card-overlay">
                     <Container className='p-0'>
                         <GreenCard />
                     </Container>
                 </div>
-
             </div>
-        </div>
+        </LoadScript>
     )
 }
